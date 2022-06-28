2c33bc31a73d2c56e99ad6dca5d95b7550a803ed
Jared Gutkin <jgutkinsam@gmail.com>
2022-06-28 18:52:17 -0500

    button event

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
-  return (
-    <header>
-        <h1>{title}</h1>
-    </header>
-  )
-}
+    const onClick = () => {
+        console.log('click')
+    }
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick}/>
+        </header>
+    )
+    }
 
 Header.defaultProps = {
     title: 'Task Tracker',
