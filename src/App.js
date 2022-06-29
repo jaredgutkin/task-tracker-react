987051fba8dcc9cb41e18e5a99e2134be95d1409
Jared Gutkin <jgutkinsam@gmail.com>
2022-06-28 21:17:08 -0500

    react icons, Task, Tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 3,
+        text: 'food shopping',
+        day: 'Feb 5th',
+        reminder: false,
+    }
+])
+
   return (
     <div className='container'>
-      <Header/>
+      <Header />
+      <Tasks tasks={tasks}/>
     </div>
   )
 }
