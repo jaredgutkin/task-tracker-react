0d5ac614c94989930380d9a145c4ab223107a9be
Jared Gutkin <jgutkinsam@gmail.com>
2022-06-30 16:19:50 -0500

    task added to dom

--- src/App.js
+++ src/App.js
@@ -15,6 +15,13 @@ function App() {
     }
 ])
 
+//Add task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
+
 //Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task)=> task.id !== id))
@@ -29,7 +36,7 @@ const toggleReminder = (id) => {
   return (
     <div className='container'>
       <Header />
-      <AddTask/>
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (
       <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}
       />) : (
